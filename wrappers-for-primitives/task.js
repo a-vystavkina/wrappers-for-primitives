6dec00f228712e16af9250acf345b5fae9fddcc4
a-vystavkina <al.vystavkina@gmail.com>
2019-11-18 20:44:48 +0300

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,4 @@
+'use strict';
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,10 +11,40 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    function checkInputData (value) {
+        let object;
+        if (typeof(value) === 'number') {
+            object = value;
+            return object;
+        } else  if (typeof (value) === 'string'){
+            object  = parseInt(value);
+        }
+        if (typeof(object) === 'number'&& ! isNaN(object)){
+            return object;
+        } else {
+          console.log(`Параметр ${value} содержит неправильное значение ${object}`);
+        }
+        
+    }
+    function loanPeriod(date) {
+      let date2 = new Date();
+      let date1 = new Date(date);
+      let year2 = date2.getFullYear();
+      let year1 = date1.getFullYear();
+      let months= (year1 - year2) * 12;
+      months += date1.getMonth() - date2.getMonth();
+      return months;
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let payMonth = loanPeriod(date);
+    let returnAmount = checkInputData(amount) - checkInputData(contribution);
+    let P = checkInputData(percent) / 1200;
+    let monthlyPayment = returnAmount * (P + (P / (((1 + P)^payMonth) - 1)));
+    let totalAmount = returnAmount + monthlyPayment * payMonth;
+    return totalAmount.toFixed(2);
+
 }
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -24,5 +55,11 @@ function sayHello() {
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting;
+    if (typeof(name) === 'string' || name != '') {
+    greeting = `Привет, мир! Меня зовут ${name}`;
+    } else if (typeof(name) != 'string' || name === '' || name === null){
+    greeting = 'Привет, мир! Меня зовут Аноним';
+    }
+    return greeting;
 }
